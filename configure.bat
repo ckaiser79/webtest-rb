aeff651329c91cb73f711e91bd56ea4c2e9b7581
unknown <gremlin@cosmopolitan.(none)>
2012-06-16 09:31:30 +0200

    rename 3rd party directories

--- configure.bat
+++ configure.bat
@@ -1,4 +1,11 @@
+
 md log
 md archived
 
-copy conf\webtest.yml.sample conf\webtest.yml
+mkdir lib\vendor
+mkdir spec\vendor
+mkdir bin-vendor
+
+IF NOT EXIST conf\webtest.yml COPY conf\webtest.yml.sample conf\webtest.yml
+
+
