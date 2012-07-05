0e5c13b8bfd0bb91d18685087e8a88b0001db2ec
Christian Kaiser <barny2k6@gmail.com>
2012-07-05 20:53:08 +0200

    usage message

--- bin/start-single-tc.bat
+++ bin/start-single-tc.bat
@@ -3,6 +3,8 @@
 IF "x%TCGROUP%" == "x" SET TCGROUP=samples
 SET TCID=%1
 
+IF "%TCID%" == "" GOTO USAGE
+
 for /f "delims=" %%a in ('ls -1 ../testcases/%TCGROUP% ^| grep %TCID%') do @set TC=%%a 
 
 :: some hacky trim function
@@ -18,3 +20,11 @@ set TC=%TC:##=%
 set WT_OPTS=
 
 ruby wetest-rb %WT_OPTS% -t "../testcases/%TCGROUP%/%TC%"
+GOTO END
+
+:USAGE
+echo start-single-tc.bat ^<testcase-spec^>
+echo testcase-spec ... name of testcase in directory ../testcases/%TCGROUP%
+echo                   may be a wildcard
+
+:END
